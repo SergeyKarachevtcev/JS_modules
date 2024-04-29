9f0d684415cd8a3a5b0ad20e3158b7f8914a6e71
SergeyKarachevtcev <estel141081@gmail.com>
2024-04-29 09:57:31 +0300

    заменил циклы на forEach

--- src/modules/tabs.js
+++ src/modules/tabs.js
@@ -3,24 +3,24 @@
 function tabs() {
   const tabBtn = document.querySelectorAll('.tablink');
 
-  for (let i = 0; i < tabBtn.length; i++) {
-    tabBtn[i].addEventListener('click', function () {
+  tabBtn.forEach((btn) => {
+    btn.addEventListener('click', function () {
       let tabName = this.dataset.tab;
       let tabContent = document.getElementById(tabName);
       let allTabContent = document.querySelectorAll('.tabcontent');
       let allTabButtons = document.querySelectorAll('.tablink');
 
-      for (let j = 0; j < allTabContent.length; j++) {
-        allTabContent[j].style.display = 'none';
-      }
+      allTabContent.forEach((content) => {
+        content.style.display = 'none';
+      });
 
-      for (let k = 0; k < allTabContent.length; k++) {
-        allTabButtons[k].classList.remove('active');
-      }
+      allTabButtons.forEach((button) => {
+        button.classList.remove('active');
+      });
 
       tabContent.style.display = 'block';
       this.classList.add('active');
     });
-  }
+  });
 }
 export default tabs;
